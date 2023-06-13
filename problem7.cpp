e168592afaadaa00587b39d103aaf8a03b07f222
youssief101 <30203051305119@sci.asu.edu.eg>
2023-06-14 02:03:53 +0300

    solve of problem 7

---
+++ problem7.cpp
@@ -0,0 +1,33 @@
+/*
+    This function should test if the factor is a factor of base.
+
+    Return true if it is a factor or false if it is not.
+
+    About factors
+    Factors are numbers you can multiply together to get another number.
+
+    2 and 3 are factors of 6 because: 2 * 3 = 6
+
+    You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+    You can use the mod operator (%) in most languages to check for a remainder
+    For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+    Note: base is a non-negative number, factor is a positive number.
+*/
+
+#include <iostream>
+using std::cout; using std::endl;
+    
+
+bool checkForFactor(int base, int factor) {
+    return base > 0 && factor >= 0 && (base % factor == 0) ? true : false;
+}
+
+int main() {
+    if (checkForFactor(7, 2)) {
+        cout << "2 is a factor of 7" << endl;
+    } else {
+        cout << "2 isn't a factor of 7" << endl;
+    }
+    return 0;
+}
